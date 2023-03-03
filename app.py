656ebd505131d7f96e5366e1a3f0e5c76a1007c0
Daniel Ferreira <daniel.ferreira.1@gmail.com>
2023-03-03 15:05:30 +0100

    add frontend

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from typing import List
 
 from fastapi import FastAPI
 from loguru import logger
+from starlette.middleware.cors import CORSMiddleware
 
 from ai_web_app.main import Result, get_embeddings, index_embeddings, search
 
@@ -16,6 +17,30 @@ if os.path.exists("./saved_index"):
 else:
     logger.info("Indexing database...")
     embeddings = index_embeddings(database_path)
+
+
+def get_cors_origin() -> str:
+    cors_origin = os.getenv("CORS_ORIGIN")
+    if cors_origin is None:
+        raise ValueError(
+            "`CORS_ORIGIN` environment variable not defined! "
+            "Please set this to the domain from which requests "
+            "to the backend will be made."
+        )
+    return cors_origin
+
+
+origins = [
+    get_cors_origin(),
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/search")
